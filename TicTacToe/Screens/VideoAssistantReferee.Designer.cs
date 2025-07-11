0d9301179dd31554b1f28d6df1c6021693f9bb9b
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-11 23:55:11 +0100

    Add VideoAssistantReferee screen
    The role of `VideoAssistantReferee` is to support loading game logs, simulating game moves, and managing user interactions, allowing for a more interactive experience.

---
+++ TicTacToe/Screens/VideoAssistantReferee.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TicTacToe.Screens
+{
+    partial class VideoAssistantReferee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listPlayedGames = new System.Windows.Forms.ListBox();
+            this.cmbFileNames = new System.Windows.Forms.ComboBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listPlayedGames
+            // 
+            this.listPlayedGames.FormattingEnabled = true;
+            this.listPlayedGames.Location = new System.Drawing.Point(554, 79);
+            this.listPlayedGames.Name = "listPlayedGames";
+            this.listPlayedGames.Size = new System.Drawing.Size(209, 212);
+            this.listPlayedGames.TabIndex = 0;
+            // 
+            // cmbFileNames
+            // 
+            this.cmbFileNames.FormattingEnabled = true;
+            this.cmbFileNames.Location = new System.Drawing.Point(593, 42);
+            this.cmbFileNames.Name = "cmbFileNames";
+            this.cmbFileNames.Size = new System.Drawing.Size(170, 21);
+            this.cmbFileNames.TabIndex = 1;
+            this.cmbFileNames.SelectionChangeCommitted += new System.EventHandler(this.cmbFileNames_SelectionChangeCommitted);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.BackgroundImage = global::TicTacToe.Properties.Resources.ButtonBg;
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnPlay.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnPlay.Location = new System.Drawing.Point(554, 297);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(209, 109);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.TabStop = false;
+            this.btnPlay.Text = "Jouer";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(551, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Date :";
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Transparent;
+            this.btnHome.BackgroundImage = global::TicTacToe.Properties.Resources.ButtonBg;
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnHome.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnHome.Location = new System.Drawing.Point(554, 390);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(209, 109);
+            this.btnHome.TabIndex = 4;
+            this.btnHome.TabStop = false;
+            this.btnHome.Text = "Revenir à l\'acceuil";
+            this.btnHome.UseVisualStyleBackColor = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // VAR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(829, 523);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.listPlayedGames);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbFileNames);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "VAR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "VAR";
+            this.Load += new System.EventHandler(this.VideoAssistantReferee_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.VAR_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listPlayedGames;
+        private System.Windows.Forms.ComboBox cmbFileNames;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHome;
+
+    }
+}
---
+++ TicTacToe/Screens/VideoAssistantReferee.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToe.Classes;
+
+namespace TicTacToe.Screens
+{
+    public partial class VideoAssistantReferee : Form
+    {
+
+        Dictionary<string, string> logFiles = null;
+        Dictionary<int, Dictionary<string, List<string>>> GameSteps;
+        Dictionary<int, string> StartingPlayers = null;
+        GameManager gm;
+        string player1Name = "", player2Name = "";
+        BackgroundWorker BW;
+        public VideoAssistantReferee()
+        {
+            InitializeComponent();
+        }
+
+        private void VideoAssistantReferee_Load(object sender, EventArgs e)
+        {
+
+            logFiles = Log.Import();
+            Log.BindTo(ref cmbFileNames);
+            GameSteps = new Dictionary<int, Dictionary<string, List<string>>>();
+            StartingPlayers = new Dictionary<int, string>();
+            gm = new GameManager(new Player("Tom"), new Player("Jerry"), this.Height);
+            cmbFileNames_SelectionChangeCommitted(cmbFileNames, null);
+            BW = new BackgroundWorker();
+            BW.DoWork += PlayGame;
+            BW.WorkerSupportsCancellation = true;
+            BW.RunWorkerCompleted += Done;
+
+
+        }
+
+        private void PlayGame(object sender, DoWorkEventArgs e)
+        {
+
+            int selectedIndex = (int)e.Argument;
+            string p1 = StartingPlayers[selectedIndex];
+            string p2 = (p1 == player1Name) ? player2Name : player1Name;
+            string lastPlayed = p1;
+            gm = new GameManager(new Player(p1), new Player(p2), this.Height, p1.ToLower() == "tom" ? true : false);
+
+
+            int step = 0;
+
+            while (step < GameSteps[selectedIndex][lastPlayed].Count)
+            {
+                System.Threading.Thread.Sleep(1000);
+                if (BW.CancellationPending)
+                {
+                    if (BW.CancellationPending) CancelBackgroundWork();
+                    return;
+                }
+                SimulateMove(selectedIndex, lastPlayed, step);
+                if (lastPlayed == p1)
+                    lastPlayed = p2;
+                else
+                {
+                    lastPlayed = p1;
+                    step++; // Incrementing the step counter after both of players are made their coresponding steps
+                }
+            }
+
+        }
+        private void CancelBackgroundWork()
+        {
+            gm.NewGame();
+            BW.DoWork -= PlayGame;
+            BW.RunWorkerCompleted -= Done;
+
+
+
+        }
+        private void Done(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (BW.CancellationPending) CancelBackgroundWork();
+            if (MessageBox.Show("Rejour ?", "Rejouer la partie", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                BW.RunWorkerAsync(listPlayedGames.SelectedIndex);
+            Repaint();
+            gm.NewGame();
+
+        }
+
+        private void SimulateMove(int GameIndex, string playerName, int stepNumber)
+        {
+
+            string cords = GameSteps[GameIndex][playerName][stepNumber];
+            Graphics g = this.CreateGraphics();
+            gm.MarkMove(int.Parse(cords.Split(',')[0]), int.Parse(cords.Split(',')[1]), ref g, false);
+
+        }
+
+
+        private void LoadLogData(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName) || GameSteps.Count > 0)
+            {
+                GameSteps.Clear();
+                StartingPlayers.Clear();
+            }
+
+            if (logFiles.ContainsKey(fileName))
+            {
+                string title = "";
+                int index = 0;
+                listPlayedGames.Items.Clear();
+                string line;
+                //List<string> Files = new List<string>();
+                using (StreamReader SR = new StreamReader(fileName))
+                {
+                    while ((line = SR.ReadLine()) != null)
+                    {
+                        if (line[0] == '>')
+                        {
+                            title = Log.ShortHeadline(line);
+                            listPlayedGames.Items.Add(title);
+                            //Files.Add(title);
+                            GameSteps.Add(index, new Dictionary<string, List<string>>());
+                            player1Name = title.Split(' ')[0].Trim();
+                            player2Name = title.Split(']')[1].Trim().Split(' ')[0].Trim();
+                            GameSteps[index].Add(player1Name, new List<string>());
+                            GameSteps[index].Add(player2Name, new List<string>());
+
+                        }
+                        else
+                        {
+                            if (line[0] != '-' && !line.ToLower().Contains("over"))
+                            {
+                                string key = line.Split(' ')[0].Trim();
+                                string steps = line.Split('[')[1].Trim().Split(']')[0].Trim();
+
+                                if (!StartingPlayers.ContainsKey(index)) StartingPlayers.Add(index, key);
+                                GameSteps[index][key].Add(steps);
+                            }
+                            if (line.ToLower().Contains("over"))
+                            {
+                                index++;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+        private void VAR_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            gm.DrawPlayboard(ref g);
+        }
+        private void Repaint()
+        {
+
+            Graphics currentGraphics = this.CreateGraphics();
+            currentGraphics.Clear(this.BackColor);
+            VAR_Paint(null, new PaintEventArgs(currentGraphics, this.ClientRectangle));
+
+
+        }
+
+        private void cmbFileNames_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            LoadLogData(((ComboBox)sender).SelectedValue.ToString());
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (listPlayedGames.SelectedIndex >= 0)
+            {
+                if (!BW.IsBusy)
+                {
+                    BW.RunWorkerAsync(listPlayedGames.SelectedIndex);
+                }
+            }
+            else
+                MessageBox.Show("Veuillez choisir d'abord la partie à jouer.", "Choix no valid", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            BW.CancelAsync();
+            UIManager.ShowHome(this.MdiParent);
+            this.Close();
+        }
+    }
+}
