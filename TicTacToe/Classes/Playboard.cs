0ae808238d3b668950c3e7a0a33acf1a07b19fbd
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-11 23:18:21 +0100

    Add Playboard and PlayboardField classes for game logic
    - Implemented Playboard class to manage a 3x3 grid of PlayboardField objects.
    - Added methods for board creation, field access, counting empty spots, and resetting fields.
    - Enhanced PlayboardField with methods for playing X or O and drawing symbols.

---
+++ TicTacToe/Classes/Playboard.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Classes
+{
+    internal class Playboard
+    {
+        static PlayboardField[,] _fields;
+        static Playboard _board;
+
+
+        private Playboard(int Size)
+        {
+            _fields = new PlayboardField[3, 3];
+            int Margin = 40;
+            int BoardSize = Size - Margin * 2;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    _fields[j, i] = new PlayboardField(new Point(i * (BoardSize / 3) + Margin, j * (BoardSize / 3) + Margin), BoardSize / 3);
+        }
+        public static Playboard CreateBord(int Size)
+        {
+            if (_board == null)
+                _board = new Playboard(Size);
+
+            return _board;
+        }
+
+        public string GetCoordinates(PlayboardField field)
+        {
+            string coordinates = "";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (_fields[i, j] == field)
+                    {
+                        coordinates = i + "," + j;
+                    }
+                }
+            }
+            return coordinates;
+        }
+
+        // -- Get a field object (the absolute way) -- 
+        public PlayboardField At(Point p)
+        {
+            PlayboardField field = null;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (_fields[i, j].PointInField(p))
+                    {
+                        return _fields[i, j];
+                    }
+                }
+            }
+            return field;
+        }
+
+        // -- Get a field object (the relative way) --
+        public PlayboardField At(int x, int y)
+        {
+            if ((x < 0 || x > 2) || (y < 0 || y > 2)) return null;
+            return _fields[x, y];
+        }
+        public int CountEmptySpots()
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (_fields[i, j].IsEmpty()) count++;
+                }
+            }
+
+            return count;
+        }
+        public int[,] ToIntegers()
+        {
+
+            int[,] integerFields = new int[3, 3];
+            PlayboardField t;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    t = _fields[i, j];
+                    integerFields[i, j] = t.IsX() ? 1 : (t.IsO() ? -1 : 0);
+                }
+            }
+
+            return integerFields;
+
+        }
+
+        public void Clean()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    _fields[i, j].Reset();
+        }
+    }
+}
---
+++ TicTacToe/Classes/PlayboardField.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.AxHost;
+
+namespace TicTacToe.Classes
+{
+    enum State { Empty = 0, X = 1, O = -1 };
+    internal class PlayboardField
+    {
+        Rectangle _rectangle;
+        State _currentState;
+        Image _image1 = null, _image2 = null;
+        static readonly int _imageMargin = 25;
+        public PlayboardField(Point Location, int Size)
+        {
+            _rectangle = new Rectangle(Location, new Size(Size, Size));
+            _currentState = State.Empty;
+
+        }
+
+        public bool PointInField(Point p)
+        {
+            return _rectangle.Contains(p);
+        }
+        public bool IsEmpty()
+        {
+            return _currentState == State.Empty;
+        }
+        public void Reset()
+        {
+            _currentState = State.Empty;
+        }
+        public bool PlayX(ref Graphics g)
+        {
+            if (IsEmpty())
+            {
+                if (_image1 != null)
+                {
+                    Image newImage = _image1;
+                    Point corner = new Point(_rectangle.Left + _imageMargin / 2, _rectangle.Top + _imageMargin / 2);
+                    g.DrawImage(newImage, corner);
+                }
+                else
+                    DrawX(ref g);
+                _currentState = State.X;
+                return true;
+            }
+            return false;
+        }
+        public bool PlayO(ref Graphics g)
+        {
+            if (IsEmpty())
+            {
+
+                if (_image2 != null)
+                {
+                    Image newImage = _image2;
+                    Point corner = new Point(_rectangle.Left + _imageMargin / 2, _rectangle.Top + _imageMargin / 2);
+                    g.DrawImage(newImage, corner);
+                }
+                else
+                    DrawO(ref g);
+                _currentState = State.O;
+                return true;
+            }
+            return false;
+        }
+
+        // -- Fallback Methods --
+        private void DrawX(ref Graphics g)
+        {
+            int Padding = 20;
+            Pen p = new Pen(Color.Brown, 15);
+            int Left = _rectangle.Left + Padding,
+                Right = _rectangle.Right - Padding,
+                Top = _rectangle.Top + Padding,
+                Bottom = _rectangle.Bottom - Padding;
+            g.DrawLine(p, Left, Top, Right, Bottom);
+            g.DrawLine(p, Right, Top, Left, Bottom);
+        }
+        private void DrawO(ref Graphics g)
+        {
+            Brush b = new SolidBrush(Color.DarkBlue);
+            int Padding = 20;
+            g.FillEllipse(b, _rectangle.Left + Padding / 2, _rectangle.Top + Padding / 2, _rectangle.Width - Padding, _rectangle.Width - Padding);
+        }
+
+        public bool IsO()
+        {
+            return _currentState == State.O;
+        }
+        public bool IsX()
+        {
+            return _currentState == State.X;
+        }
+    }
+}
