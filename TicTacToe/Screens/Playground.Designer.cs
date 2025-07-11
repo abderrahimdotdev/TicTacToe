cad40ab6df9b9a233adeedba2d2efb41b902a764
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-11 23:48:23 +0100

    Add Playground screen

---
+++ TicTacToe/Screens/Playground.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace TicTacToe.Screens
+{
+    partial class Playground
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Player1Score = new System.Windows.Forms.Label();
+            this.Player2Score = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Player2Name = new System.Windows.Forms.Label();
+            this.Player1Name = new System.Windows.Forms.Label();
+            this.lblArrowPlayer1 = new System.Windows.Forms.Label();
+            this.lblArrowPlayer2 = new System.Windows.Forms.Label();
+            this.JerryPic = new System.Windows.Forms.PictureBox();
+            this.TomPic = new System.Windows.Forms.PictureBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.JerryPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TomPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(1234, 162);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 72);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score";
+            // 
+            // Player1Score
+            // 
+            this.Player1Score.AutoSize = true;
+            this.Player1Score.BackColor = System.Drawing.Color.Transparent;
+            this.Player1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F);
+            this.Player1Score.ForeColor = System.Drawing.Color.Sienna;
+            this.Player1Score.Location = new System.Drawing.Point(1184, 369);
+            this.Player1Score.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Player1Score.Name = "Player1Score";
+            this.Player1Score.Size = new System.Drawing.Size(78, 85);
+            this.Player1Score.TabIndex = 3;
+            this.Player1Score.Text = "0";
+            // 
+            // Player2Score
+            // 
+            this.Player2Score.AutoSize = true;
+            this.Player2Score.BackColor = System.Drawing.Color.Transparent;
+            this.Player2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F);
+            this.Player2Score.ForeColor = System.Drawing.Color.SlateGray;
+            this.Player2Score.Location = new System.Drawing.Point(1380, 369);
+            this.Player2Score.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Player2Score.Name = "Player2Score";
+            this.Player2Score.Size = new System.Drawing.Size(78, 85);
+            this.Player2Score.TabIndex = 4;
+            this.Player2Score.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1286, 369);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 73);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "-";
+            // 
+            // Player2Name
+            // 
+            this.Player2Name.AutoSize = true;
+            this.Player2Name.BackColor = System.Drawing.Color.Transparent;
+            this.Player2Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2Name.ForeColor = System.Drawing.Color.SlateGray;
+            this.Player2Name.Location = new System.Drawing.Point(1378, 471);
+            this.Player2Name.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Player2Name.Name = "Player2Name";
+            this.Player2Name.Size = new System.Drawing.Size(56, 48);
+            this.Player2Name.TabIndex = 7;
+            this.Player2Name.Text = "...";
+            // 
+            // Player1Name
+            // 
+            this.Player1Name.AutoSize = true;
+            this.Player1Name.BackColor = System.Drawing.Color.Transparent;
+            this.Player1Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player1Name.ForeColor = System.Drawing.Color.Sienna;
+            this.Player1Name.Location = new System.Drawing.Point(1180, 471);
+            this.Player1Name.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Player1Name.Name = "Player1Name";
+            this.Player1Name.Size = new System.Drawing.Size(56, 48);
+            this.Player1Name.TabIndex = 8;
+            this.Player1Name.Text = "...";
+            // 
+            // lblArrowPlayer1
+            // 
+            this.lblArrowPlayer1.AutoSize = true;
+            this.lblArrowPlayer1.BackColor = System.Drawing.Color.Transparent;
+            this.lblArrowPlayer1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrowPlayer1.ForeColor = System.Drawing.Color.Sienna;
+            this.lblArrowPlayer1.Location = new System.Drawing.Point(1138, 465);
+            this.lblArrowPlayer1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblArrowPlayer1.Name = "lblArrowPlayer1";
+            this.lblArrowPlayer1.Size = new System.Drawing.Size(59, 59);
+            this.lblArrowPlayer1.TabIndex = 9;
+            this.lblArrowPlayer1.Text = ">>";
+            this.lblArrowPlayer1.Visible = false;
+            // 
+            // lblArrowPlayer2
+            // 
+            this.lblArrowPlayer2.AutoSize = true;
+            this.lblArrowPlayer2.BackColor = System.Drawing.Color.Transparent;
+            this.lblArrowPlayer2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrowPlayer2.ForeColor = System.Drawing.Color.SlateGray;
+            this.lblArrowPlayer2.Location = new System.Drawing.Point(1336, 465);
+            this.lblArrowPlayer2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblArrowPlayer2.Name = "lblArrowPlayer2";
+            this.lblArrowPlayer2.Size = new System.Drawing.Size(59, 59);
+            this.lblArrowPlayer2.TabIndex = 10;
+            this.lblArrowPlayer2.Text = ">>";
+            this.lblArrowPlayer2.Visible = false;
+            // 
+            // JerryPic
+            // 
+            this.JerryPic.Image = global::TicTacToe.Properties.Resources.Jerry;
+            this.JerryPic.Location = new System.Drawing.Point(1108, 288);
+            this.JerryPic.Margin = new System.Windows.Forms.Padding(6);
+            this.JerryPic.Name = "JerryPic";
+            this.JerryPic.Size = new System.Drawing.Size(200, 67);
+            this.JerryPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.JerryPic.TabIndex = 12;
+            this.JerryPic.TabStop = false;
+            // 
+            // TomPic
+            // 
+            this.TomPic.Image = global::TicTacToe.Properties.Resources.Tom;
+            this.TomPic.Location = new System.Drawing.Point(1346, 288);
+            this.TomPic.Margin = new System.Windows.Forms.Padding(6);
+            this.TomPic.Name = "TomPic";
+            this.TomPic.Size = new System.Drawing.Size(138, 75);
+            this.TomPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TomPic.TabIndex = 11;
+            this.TomPic.TabStop = false;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.Transparent;
+            this.btnNewGame.BackgroundImage = global::TicTacToe.Properties.Resources.ButtonBg;
+            this.btnNewGame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNewGame.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNewGame.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnNewGame.FlatAppearance.BorderSize = 0;
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewGame.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnNewGame.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnNewGame.Location = new System.Drawing.Point(1108, 569);
+            this.btnNewGame.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(418, 210);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.TabStop = false;
+            this.btnNewGame.Text = "Reset Scores";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Transparent;
+            this.btnHome.BackgroundImage = global::TicTacToe.Properties.Resources.ButtonBg;
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnHome.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnHome.Location = new System.Drawing.Point(1108, 750);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(6);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(418, 210);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.TabStop = false;
+            this.btnHome.Text = "Back to Home Screen";
+            this.btnHome.UseVisualStyleBackColor = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // Playground
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(1658, 1006);
+            this.Controls.Add(this.JerryPic);
+            this.Controls.Add(this.TomPic);
+            this.Controls.Add(this.Player1Name);
+            this.Controls.Add(this.Player2Name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Player2Score);
+            this.Controls.Add(this.Player1Score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.lblArrowPlayer2);
+            this.Controls.Add(this.lblArrowPlayer1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Playground";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Playground_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Playground_Paint);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Playground_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.JerryPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TomPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Player1Score;
+        private System.Windows.Forms.Label Player2Score;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Player2Name;
+        private System.Windows.Forms.Label Player1Name;
+        private System.Windows.Forms.Label lblArrowPlayer1;
+        private System.Windows.Forms.Label lblArrowPlayer2;
+        private System.Windows.Forms.PictureBox TomPic;
+        private System.Windows.Forms.PictureBox JerryPic;
+    }
+}
+
---
+++ TicTacToe/Screens/Playground.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToe.Classes;
+
+namespace TicTacToe.Screens
+{
+    public partial class Playground : Form
+    {
+        GameManager gameManager;
+        Player p1;
+        Player p2;
+        public Playground()
+        {
+            InitializeComponent();
+        }
+
+        private void Playground_Load(object sender, EventArgs e)
+        {
+            bool vsPC = this.Tag.ToString() == "1" ? true : false;
+            p1 = new Player("Jerry");
+            p2 = new Player("Tom");
+            gameManager = new GameManager(p1, p2, this.Height, vsPC);
+            gameManager.OnNextMove += UpdateUI;
+            SetPlayersName();
+            HighLightPlayerName();
+
+
+
+
+        }
+        private void SetPlayersName()
+        {
+            Player1Name.Text = p1.GetName();
+            Player2Name.Text = p2.GetName();
+        }
+        private void RefreshPlayersScore()
+        {
+            Player1Score.Text = p1.GetScore().ToString();
+            Player2Score.Text = p2.GetScore().ToString();
+        }
+        private void UpdateUI(GameProgress progress)
+        {
+            switch (progress)
+            {
+                case GameProgress.Over:
+                    RefreshPlayersScore();
+                    gameManager.NewGame();
+                    Repaint();
+                    break;
+                case GameProgress.Reset:
+                    RefreshPlayersScore();
+                    Repaint();
+                    break;
+                case GameProgress.InProgress:
+
+                    break;
+                case GameProgress.Draw:
+                    gameManager.NewGame();
+                    Repaint();
+                    break;
+            }
+            HighLightPlayerName();
+        }
+        private void HighLightPlayerName()
+        {
+            Label arrowPlayer1 = lblArrowPlayer1;
+            Label arrowPlayer2 = lblArrowPlayer2;
+            if (arrowPlayer1.Visible)
+            {
+                arrowPlayer1.Visible = false;
+                arrowPlayer2.Visible = true;
+            }
+            else
+            {
+                arrowPlayer1.Visible = true;
+                arrowPlayer2.Visible = false;
+            }
+
+        }
+        private void Playground_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            gameManager.DrawPlayboard(ref g);
+
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            gameManager.NewGame();
+            UIManager.ShowHome(this.MdiParent);
+            this.Close();
+        }
+
+        private void Playground_MouseUp(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            gameManager.MarkMove(e.Location, ref g);
+
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            gameManager.NewGame(true);
+            Repaint();
+        }
+        private void Repaint()
+        {
+            Graphics currentGraphics = this.CreateGraphics();
+            currentGraphics.Clear(this.BackColor);
+            Playground_Paint(null, new PaintEventArgs(currentGraphics, this.ClientRectangle));
+        }
+    }
+}
