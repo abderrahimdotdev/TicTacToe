a316e54ec892914e291cbcafde5b8931080e07bf
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-11 23:12:56 +0100

    Add Player class for managing player data and scores
    Introduces a new `Player` class in the `TicTacToe.Classes` namespace. The class includes fields for the player's score and name, a constructor for initialization, and methods to manage the score and retrieve player information.

---
+++ TicTacToe/Classes/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Classes
+{
+    internal class Player
+    {
+        int _score;
+        string _name;
+
+        public Player(string PlayerName)
+        {
+            if (string.IsNullOrWhiteSpace(PlayerName.Trim())) throw new ArgumentNullException("PlayerName", "All players must have a name");
+            _name = PlayerName;
+            _score = 0;
+        }
+        public void Win()
+        {
+            _score++;
+        }
+        public void ResetScore()
+        {
+            _score = 0;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetScore()
+        {
+            return _score;
+        }
+    }
+}
